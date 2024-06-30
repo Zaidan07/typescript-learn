86716b02b54c316ec109a8608d82d00598f583db
Zaidan07 <zaidanxgaming@gmail.com>
2024-06-30 20:15:20 +0700

    feat: second commit

--- index.ts
+++ index.ts
@@ -1,1 +1,26 @@
-"hellow"
+class OrangTua {
+   public Nama = 'Idah';
+   public Rambut = 'hitam';
+
+    constructor (nama: string){
+        this.Nama = nama
+    }
+
+    sapaan () {
+        console.log(`Halo perkenalkan Nama saya ${this.Nama}`)
+    }
+    
+}
+
+class Anak extends OrangTua {
+
+    constructor (nama: string) {
+        super(nama)
+    }
+
+
+    pekerjaan () {
+        console.log('Saya seorang petani')
+    }
+}
+console.log(new Anak('Jono').sapaan())
